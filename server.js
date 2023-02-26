17ef0ca68b4858932b0ab29cf1f89a56965bbb5e
Joel Solera <joelss084@gmail.com>
2023-02-25 20:30:37 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routes/contactRoute");
+const errorHandler = require("./middleWare/errorMiddleware");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+const app = express();
+
+//Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors({ origin: ["http://127.0.0.1:5173"], credentials: true }));
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+//Routes Middlewares
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/contactus", contactRoute);
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+//Error Middleware
+app.use(errorHandler);
+
+//CONNECT TO DB AND START SERVER
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(`mongodb://${process.env.MONGO_HOST}/${process.env.MONGO_DATABASE}`)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Running On PORT ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
